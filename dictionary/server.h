90def6f329a12410cb9dfad385f82d4abdc7e9e5
Nemanja Boric <4burgos@gmail.com>
2017-07-16 19:48:40 +0200

    Add boost license and original copyright notices

--- dictionary/server.h
+++ dictionary/server.h
@@ -1,3 +1,10 @@
+// Copyright (c) 2003-2016 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+// Copyright (c) 2017 Nemanja Boric (4burgos _at_ _gmail_ _com_)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
 #pragma once
 
 #include <boost/asio.hpp>
